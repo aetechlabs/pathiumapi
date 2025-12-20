b14cd3b693d084f5ef07f02137a7ee6303b83d20
Yipmong Said <Syipmong5@gmail.com>
2025-12-20 13:33:33 +0100

    docs: add README and example app

---
+++ examples/app.py
@@ -0,0 +1,17 @@
+from lilac import Lilac, Response, logging_middleware_factory, error_middleware
+
+app = Lilac()
+
+# register middleware
+app.use(logging_middleware_factory(print))
+app.use(error_middleware)
+
+@app.get("/")
+async def index(req):
+    return "Hello from Lilac"
+
+@app.get("/items/{id:int}")
+async def get_item(req, id):
+    return Response.json({"item_id": id})
+
+# expose `app` for ASGI servers like uvicorn
